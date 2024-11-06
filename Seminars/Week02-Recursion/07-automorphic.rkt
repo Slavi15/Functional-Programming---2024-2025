d6c3eb8ec12952bf188a14f74eb86ad3a8c7d8b6
Slavi Rezashki <57730123+Slavi15@users.noreply.github.com>
2024-11-06 11:37:22 +0200

    Update Week02

--- Seminars/Week02-Recursion/07-automorphic.rkt
+++ Seminars/Week02-Recursion/07-automorphic.rkt
@@ -3,8 +3,29 @@
 (define // quotient)
 (define % modulo)
 
-(define (square x) (* x x))
+(define (sq x) (* x x))
+
+(define (count-digits n)
+  (define (iter sum num)
+    (if (zero? num)
+        sum
+        (iter (+ 1 sum) (// num 10))
+        )
+    )
+  (iter 0 n)
+  )
+
+(define (get-last-digits n k)
+  (% n (expt 10 k))
+  )
 
 (define (automorphic? n)
-  (= n (% (square n) 10))
-)
+  (let ([sq-num (sq n)]
+        [k (count-digits n)])
+    (= n (get-last-digits sq-num k))
+    )
+  )
+
+(automorphic? 5)
+(automorphic? 25)
+(automorphic? 7)
